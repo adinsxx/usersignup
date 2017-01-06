f4c672966cd5f72ef2c0738709fe91baf8623fb0
Krishna Santhanam <krishna@yaanatech.com>
2017-01-06 15:45:00 -0800

    Initial checkin for Quiz:Registration.

--- usersignup.py
+++ usersignup.py
@@ -12,11 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import webapp2, cgi,re
+import webapp2, cgi,re, hashlib, hmac, random, string
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 PASSWORD_RE = re.compile(r"^.{3,20}$")
 EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+SECRET = 'imsosecret'
+
+def hash_str(s):
+	return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+	return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+	val = h.split('|')[0]
+	if h == make_secure_val(val):
+		return val
+        
+def make_salt():
+    valid_letters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    return ''.join((random.choice(valid_letters) for i in xrange(5)))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name, pw, salt)
 
 form="""
 <!DOCTYPE html>
@@ -117,17 +144,34 @@ class MainPage(webapp2.RequestHandler):
         passworderror = "" if password else "That wasn't a valid password."
         verifyerror = "" if passworderror else "" if user_password == user_verify else "Your passwords didnt match."
         emailerror = "" if email else "That's not a valid email."
+        
+        self.response.headers['Content-Type'] = 'text/plain'
+        usernameCookie = ''
+        username_cookie_str = self.request.cookies.get('username')
+        if username_cookie_str:
+        	username_cookie_val = check_secure_val(username_cookie_str)
+        	if username_cookie_val:
+        		usernameCookie = username_cookie_val
+
+        #visits += 1
+
+        new_username_cookie_val = make_secure_val(usernameCookie)
+
+        self.response.headers.add_header('Set-Cookie', 'username=%s' % new_username_cookie_val)
 
         if (not usernameerror and not passworderror and not verifyerror and not emailerror):
-          self.redirect("/unit2/welcome?username=" + user_username)
+          self.redirect("/unit2/welcome")
         else:
           self.write_form(usernameerror, passworderror, verifyerror, emailerror, user_username, user_email)
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-    	username = self.request.get('username')
+    	username_cookie_str = self.request.cookies.get('username')
+        username_cookie_val = check_secure_val(username_cookie_str)
+        if username_cookie_val:
+            usernameCookie = username_cookie_val
     	if valid_username(username):
-        	self.response.out.write("<!DOCTYPE html><html><head><title>Unit 2 Signup</title></head><body><h2>Welcome, " + username + "!</h2></body></html>")
+        	self.response.out.write("<!DOCTYPE html><html><head><title>Unit 2 Signup</title></head><body><h2>Welcome, " + usernameCookie + "!</h2></body></html>")
         else:
         	self.redirect('/unit2/signup')
 
